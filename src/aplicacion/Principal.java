70fe7490fe7720189c18ee4b5284b1e0b300b59f
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 16:15:39 +0100

    Ejercicio8 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.*;
+
+import static dominio.Ejercicio8.sumas;
+
+public class Principal {
+    public static void main(String[] args){
+        int[]lista ={1,2,4,3,56,4,24,2,8,43};
+        System.out.print(" "+sumas(lista, lista.length-1));
+    }
 }
--- src/dominio/Ejercicio8.java
+++ src/dominio/Ejercicio8.java
@@ -1,2 +1,19 @@
-package dominio;public class Ejercicio8 {
+package dominio;
+
+public class Ejercicio8 {
+    public static int sumas(int lista[],int n){
+        if(n==0){
+            if(lista[n]%2==0){
+                return lista[n];
+            }else{
+                return 0;
+            }
+        }
+        if(n!=0){
+            if(lista[n]%2!=0){
+                return sumas(lista,n-1);
+            }
+        }
+        return sumas(lista,n-1)+lista[n];
+    }
 }
